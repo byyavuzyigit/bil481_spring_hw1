ab4f77f9cd4a729df7b8096f23729add242b6c74
byyavuzyigit <byavuzyigit@etu.edu.tr>
2024-03-08 10:57:14 +0300

    getting ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,11 +2,13 @@ package com.mycompany.app;
 
 import java.util.*;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
 
-/**
- * Hello world!
- *
- */
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 public class App 
 {
     // iterates through both arraylists and appends the corresponding text
@@ -52,4 +54,69 @@ public class App
         return result;
     }
 
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+            //System.out.println(req.queryParams("input1"));
+            //System.out.println(req.queryParams("input2"));
+
+            String input1 = req.queryParams("input1");
+            Scanner sc1 = new java.util.Scanner(input1);
+            sc1.useDelimiter("[;\r\n]+");
+            ArrayList<Integer> list1 = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+                list1.add(value);
+            }
+            System.out.println(list1);
+
+
+            String input2 = req.queryParams("input2");
+            Scanner sc2 = new java.util.Scanner(input2);
+            sc2.useDelimiter("[;\r\n]+");
+            ArrayList<Integer> list2 = new java.util.ArrayList<>();
+            while (sc2.hasNext())
+            {
+                int value = Integer.parseInt(sc2.next().replaceAll("\\s",""));
+                list2.add(value);
+            }
+            System.out.println(list2);
+
+            String input3 = req.queryParams("input3");
+            input3 = input3 != null ? input3.replaceAll("\\s","") : "";
+
+            String input4 = req.queryParams("input4");
+            input4 = input4 != null ? input4.replaceAll("\\s","") : "";
+
+            String result = concatText(list1, list2, input3, input4);
+
+            Map map = new HashMap();
+            map.put("result", result);
+            return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+                (rq, rs) -> {
+                    Map map = new HashMap();
+                    map.put("result", "not computed yet!");
+                    return new ModelAndView(map, "compute.mustache");
+                },
+                new MustacheTemplateEngine());
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
+
+
+
